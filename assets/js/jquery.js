a849b91bf8d5ccc73a97a0f1e9ce7c87b8d36e62
Rick Warling <rickwarling@gmail.com>
2018-02-12 22:04:01 +0100

    major changes #4

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,5 +1,34 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    $("a.nav-link").click(function() {
+       $(".navbar-toggler").attr("aria-expanded", "false");
+       $(".navbar-toggler").addClass("collapsed");
+       $('.navbar-collapse').removeClass('show');
+
+    });
+
+    var scroll = $(document).scrollTop();
+        var headerHeight = $('.page-header').outerHeight();
+
+        $(window).scroll(function() {
+          var scrolled = $(document).scrollTop();
+          if (scrolled > headerHeight){
+            $('.page-header').addClass('off-canvas');
+          } else {
+            $('.page-header').removeClass('off-canvas');
+          }
+
+            if (scrolled > scroll){
+             $('.page-header').removeClass('fixed');
+            } else {
+            $('.page-header').addClass('fixed');
+            }
+          scroll = $(document).scrollTop();
+         });
+
+
+
     $('.popup-with-zoom-anim').magnificPopup({
 		type: 'inline',
 
